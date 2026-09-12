99786e811c7670a9d3de5118c280e47eb614492d
Devilalitha <devi9750006689@gmail.com>
2026-09-12 12:23:44 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+      int n=nums.size();
+      int longest=0;
+      unordered_set<int>st(nums.begin(),nums.end());
+      for(int i=0;i<n;i++){
+        if(st.find(nums[i]-1)==st.end()){
+            int length=0;
+            while(st.find(nums[i]+length)!=st.end()){
+                length++;
+            }
+            longest=max(longest,length);
+        }
+      }
+      return longest;
+    }
+};
